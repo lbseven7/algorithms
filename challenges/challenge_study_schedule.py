bfefb3715aab3a884cf79fae1657fccf55fe4a74
lbseven7 <leobarbosapinturas@gmail.com>
2023-01-27 15:58:01 -0300

    feat: Número de estudantes estudando no mesmo horário (Algoritmo de busca)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
+    if target_time is None:
+        return None
+    contador = {}
+    for entrada, saida in permanence_period:
+        if entrada <= target_time <= saida:
+            if target_time in contador:
+                contador[target_time] += 1
+            else:
+                contador[target_time] = 1
+    return max(contador, key=contador.get)
     raise NotImplementedError
