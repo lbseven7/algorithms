7448d5280e797678454cf17ad1cb2c62d0f1fa9a
lbseven7 <leobarbosapinturas@gmail.com>
2023-01-31 14:03:55 -0300

    feat: Implementa teste da função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -5,7 +5,7 @@ import pytest
 def test_encrypt_message():
     with pytest.raises(TypeError):
         encrypt_message(1, "2") or encrypt_message("message", "key")
-    
+
     assert encrypt_message("message", 3) == "sem_egas"
     assert encrypt_message("test", 1) == "t_tse"
     assert encrypt_message("message", -1) == "egassem"
